fc80c6b52bdb6b35fccda8f094bd0ae26432477c
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2014-10-30 09:51:23 +0300

    Шапку вернул

--- rutracker.org-magnet.user.js
+++ rutracker.org-magnet.user.js
@@ -1,11 +1,15 @@
 // ==UserScript==
-// @name         rutracker.org magnet
-// @version      1.0
-// @description  Добавляет на страницу с раздачей magnet-ссылку (там где хэш)
-// @author       alezhu
+// @id             rutracker.org-magnet
+// @name           rutracker.org magnet
+// @version        1.0
+// @author         alezhu
+// @description    Добавляет на страницу с раздачей magnet-ссылку (там где хэш)
+// @include        http://*.rutracker.org/* 
+// @include        https://*.rutracker.org/*
+// @include        http://rutracker.org/* 
+// @include        https://rutracker.org/*
 // @match          *://rutracker.org/*   
-// @match          *://*.rutracker.org/* 
-// @grant        none
+// @match          *://*.rutracker.org/*   
 // ==/UserScript==
 (function(){
 	var LOG = 0;
