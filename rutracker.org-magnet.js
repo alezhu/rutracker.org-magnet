60f1c3783bc4c24069250321276544c5dd52d8fa
Alexandr Zhuravlev <alexandr.zhuravlev@gmail.com>
2014-10-30 09:33:28 +0300

    заменена шапка

--- rutracker.org-magnet.js
+++ rutracker.org-magnet.js
@@ -1,15 +1,11 @@
 // ==UserScript==
-// @id             rutracker.org-magnet
-// @name           rutracker.org magnet
-// @version        1.0
-// @author         alezhu
-// @description    Добавляет на страницу с раздачей magnet-ссылку (там где хэш)
-// @include        http://*.rutracker.org/* 
-// @include        https://*.rutracker.org/*
-// @include        http://rutracker.org/* 
-// @include        https://rutracker.org/*
+// @name         rutracker.org magnet
+// @version      1.0
+// @description  Добавляет на страницу с раздачей magnet-ссылку (там где хэш)
+// @author       alezhu
 // @match          *://rutracker.org/*   
-// @match          *://*.rutracker.org/*   
+// @match          *://*.rutracker.org/* 
+// @grant        none
 // ==/UserScript==
 (function(){
 	var LOG = 0;
